fe93d211cded291ac5bbd2e77ce39378806ce732
Dennis Schenkel <dennisschenkel@googlemail.com>
2024-09-01 13:50:38 +0200

    MS-3 Add user profile information to tool api call

--- tools/serializers.py
+++ tools/serializers.py
@@ -1,12 +1,14 @@
 from rest_framework import serializers
 from .models import Tool
 from categories.serializers import CategorySerializer
+from profiles.serializers import ProfileSerializer
 
 
 class ToolSerializer(serializers.ModelSerializer):
     user = serializers.ReadOnlyField(source="user.username")
     is_owner = serializers.SerializerMethodField()
     category = CategorySerializer(many=True, read_only=True)
+    profile = ProfileSerializer(source='user.profile', read_only=True)
 
     def get_is_owner(self, obj):
         """
@@ -26,6 +28,7 @@ class ToolSerializer(serializers.ModelSerializer):
         fields = [
             "id",
             "user",
+            "profile",
             "title",
             "short_description",
             "full_description",
